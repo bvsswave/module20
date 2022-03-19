96a03fc7930b0742a67cfa613f24adaedb72b1f4
bvsswave <tannershahan@gmail.com>
2022-03-18 22:34:46 -0600

    Updated code

--- src/App.js
+++ src/App.js
@@ -6,13 +6,13 @@ import ContactForm from './components/Contact';
 
 function App() {
   const [categories] = useState([
-    {
-      name: 'commercial',
-      description: 'Photos of grocery stores, food trucks, and other commercial projects',
-    },
-    { name: 'portraits', description: 'Portraits of people in my life' },
-    { name: 'food', description: 'Delicious delicacies' },
-    { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
+    // {
+    //   name: 'commercial',
+    //   description: 'Photos of grocery stores, food trucks, and other commercial projects',
+    // },
+    { name: 'projects', description: 'Projects by me' },
+    // { name: 'food', description: 'Delicious delicacies' },
+    // { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
   ]);
 
   const [currentCategory, setCurrentCategory] = useState(categories[0]);
@@ -29,6 +29,9 @@ function App() {
         setContactSelected={setContactSelected}
       ></Nav>
       <main>
+        <div>
+          
+        </div>
         {!contactSelected ? (
           <>
             <Gallery currentCategory={currentCategory}></Gallery>
