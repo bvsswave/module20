d880f8b9567b1dbced490ca1e209a292e95b9307
bvsswave <tannershahan@gmail.com>
2022-03-18 20:53:31 -0600

    Adding tests

---
+++ src/components/About/__tests__/index.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import About from '..';
+
+const { asFragment } = render(<About />);
+
+expect(asFragment()).toMatchSnapshot();
+
+describe('About component', () => {
+    // First Test
+    it('matches snapshot DOM node structure', () => {
+        // render About
+      });
+  
+    // Second Test
+  })
+
+
+  
+afterEach(cleanup);
